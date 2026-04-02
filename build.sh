fcfd8ffbc53277475e581f9582778793f8299263
bbqsauce <metallicadude9@gmail.com>
2026-04-01 23:12:18 -0500

    Update build.sh for pretty banner...

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ sed -n '/BEGIN_GRUBCFG/,/END_GRUBCFG/p' kernel.c | sed '1d;$d' > grub.cfg
 
 mkdir -p build iso/boot/grub
 
-gcc -m32 -ffreestanding -c kernel.c -o build/kernel.o
+gcc -m32 -fno-stack-protector -fno-pie -fno-pic \
+    -finput-charset=utf-8 -fexec-charset=cp437 \
+    -c kernel.c -o build/kernel.o
+
 ld -m elf_i386 -T linker.ld -o build/kernel.elf build/kernel.o
 
 cp build/kernel.elf iso/boot/kernel.elf
